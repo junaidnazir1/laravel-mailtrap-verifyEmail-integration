cddd2361bebcba2a14c58a3a3b248edbf467e991
Junaid Nazir <junaidahmad@thesupportboxbits.com>
2020-07-19 10:35:11 +0530

    laravel-mailtrap-integration-done

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\verifyMail;
+use Log;
+use Carbon\Carbon;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('verify');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+        $user= User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'remember_token' =>sha1(time())
+        ]);
+        Mail::to($user['email'])->send( new verifyMail($user));
+
+        return $user;
+    }
+
+    public function verify($token)
+    {
+        $verifyUser=user::where('remember_token', $token)->first();
+        log::info($verifyUser);
+        if(!$verifyUser['email_verified_at'])
+        {
+           $verifyUser['email_verified_at']=Carbon::now();
+           log::info($verifyUser);
+           $verifyUser->save();
+           $status= "Email Verified";
+        } else {
+            $status= "Email not verified";
+        }
+        return view('emails.emailStatus')->with('status', $status);
+    }
+   
+}
---
+++ resources/views/emails/emailStatus.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Welcome Email</title>
+</head>
+
+<body>
+    <H1>{{$status}}</H1>
+</body>
+
+</html>
